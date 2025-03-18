b993102441184832d1f4de290eb6d5e26783f265
LvPeng990324 <lvpeng990324@gmail.com>
2025-03-18 21:45:56 +0800

    用户日志记录的同时记录系统日志

--- utils/user_log.py
+++ utils/user_log.py
@@ -1,11 +1,16 @@
 from SystemManagement.models import UserLog
 
+from utils.logger import write_log
+
 
 def add_user_log(request, action, detail):
     """ 增加用户日志
     """
+    user_id = request.session.get('user_id')
+    write_log('info', f'add_user_log {user_id} {action} {detail}')
+
     UserLog.objects.create(
-        user_id=request.session.get('user_id'),
+        user_id=user_id,
         action=action,
         detail=detail,
     )
