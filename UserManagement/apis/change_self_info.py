e2da17abe4cf1284a378fcd17b1b2106616f03da
LvPeng990324 <lvpeng990324@gmail.com>
2025-03-18 22:56:53 +0800

    加编辑自己信息接口

---
+++ UserManagement/apis/change_self_info.py
@@ -0,0 +1,53 @@
+from UserManagement.models import User
+
+from utils.custom_response import ERROR_CODE
+from utils.custom_response import json_response
+from utils.user_log import add_user_log
+from utils.data_covert import str_to_md5
+
+
+def change_self_info(request):
+    """ 更改自己信息
+    POST请求
+    """
+    name = request.json.get('name')
+    phone = request.json.get('phone')
+    new_password = request.json.get('new_password')
+    new_password_md5 = str_to_md5(new_password or '')
+
+    try:
+        user = User.objects.get(id=request.session.get('user_id'))
+    except User.DoesNotExist:
+        return json_response(code=ERROR_CODE.NOT_FOUND, data={
+            "msg": "未找到当前用户，重新登录",
+        })
+
+    # 记录本次修改的内容描述
+    edit_log_str = ''
+
+    # 按需修改
+    if user.name != name:
+        edit_log_str += f'姓名：{user.name} -> {name}\n'
+        user.name = name
+
+    if user.phone != phone:
+        edit_log_str += f'手机号：{user.phone} -> {phone}\n'
+        user.phone = phone
+
+    # 密码是给了才修改
+    if new_password and user.password_md5 != new_password_md5:
+        edit_log_str += '修改了密码\n'
+        user.password_md5 = new_password_md5
+
+    user.save()
+
+    # 记录用户日志
+    add_user_log(
+        request=request,
+        action='修改自己信息',
+        detail=edit_log_str,
+    )
+
+    return json_response(code=ERROR_CODE.SUCCESS, data={
+        "username": "admin11",
+    })
--- UserManagement/urls.py
+++ UserManagement/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from UserManagement.apis.login import login
 from UserManagement.apis.get_user_info import get_user_info
+from UserManagement.apis.change_self_info import change_self_info
 
 
 app_name = 'UserManagement'
@@ -11,4 +12,6 @@ urlpatterns = [
     path('login/', login, name='login'),
     # 获取用户信息
     path('get-user-info/', get_user_info, name='get_user_info'),
+    # 更改自己信息
+    path('change-self-info/', change_self_info, name='change_self_info'),
 ]
