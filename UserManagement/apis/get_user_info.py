461816005d52ca90e1c431bf2955f876e3c3c750
Lv Peng <lvpeng990324@qq.com>
2025-03-26 11:38:21 +0800

    获取用户信息接口逻辑修改

--- UserManagement/apis/get_user_info.py
+++ UserManagement/apis/get_user_info.py
@@ -1,3 +1,5 @@
+from UserManagement.models import User
+
 from utils.custom_response import ERROR_CODE
 from utils.custom_response import json_response
 from utils.permission_check import login_required
@@ -8,6 +10,9 @@ def get_user_info(request):
     """ 获取用户信息
     GET请求
     """
+    user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)
+
     return json_response(code=ERROR_CODE.SUCCESS, data={
-        "username": "admin11",
+        "username": user.name,
     })
